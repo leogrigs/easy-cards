bd3b24936584066fd8b668b1478c9ce0e4a4e9ee
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-11-25 20:27:37 -0300

    refactor: landing page using AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { AuthProvider } from "./auth/AuthContext";
+import { AuthProvider } from "../providers/AuthContext";
 import "./globals.css";
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { LogIn } from "lucide-react";
+import { useAuth } from "../providers/AuthContext";
+
 export default function Home() {
+  const { loginWithGoogle } = useAuth();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl md:text-6xl font-bold text-center animate-pulse">
-          Easy Cards
-        </h1>
-      </main>
+    <div className="min-h-screen bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-50 flex items-center justify-center">
+      <div className="w-full max-w-4xl px-6 py-16 sm:py-24 animate-fade-in">
+        {/* Hero Section */}
+        <header className="text-center">
+          <h1 className="text-5xl sm:text-6xl font-extrabold tracking-tight leading-tight mb-6">
+            Easy Cards
+          </h1>
+          <p className="text-lg sm:text-xl text-zinc-600 dark:text-zinc-400 max-w-2xl mx-auto mb-10">
+            Simplify learning with <strong>Easy Cards</strong> — a smart,
+            intuitive app for creating, managing, and sharing flashcards.
+          </p>
+          <Button onClick={loginWithGoogle} variant="link">
+            <LogIn />
+            Sign in with Google
+          </Button>
+        </header>
+
+        {/* Features Section */}
+        <section className="mt-16 grid gap-8 sm:grid-cols-2">
+          {[
+            {
+              title: "Create Flashcards",
+              text: "Quickly create customizable flashcards for any topic.",
+            },
+            {
+              title: "Explore Community",
+              text: "Access a library of public flashcards from peers worldwide.",
+            },
+          ].map((feature, index) => (
+            <div
+              key={index}
+              className="p-6 border border-zinc-200 dark:border-zinc-800 rounded-lg bg-transparent shadow-md"
+            >
+              <h3 className="text-xl font-bold mb-2">{feature.title}</h3>
+              <p className="text-zinc-600 dark:text-zinc-400">{feature.text}</p>
+            </div>
+          ))}
+        </section>
+      </div>
     </div>
   );
 }
