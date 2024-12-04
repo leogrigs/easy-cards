747bf4d25352886ecc88fae28a943964c8da0bcd
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-04 09:36:09 -0300

    refactor: module and module preview interfaces

--- interfaces/module.interface.ts
+++ interfaces/module.interface.ts
@@ -1,15 +1,13 @@
 import { ICard } from "./card.interface";
 
-export interface Module {
-  id: string;
-  name: string;
-  public: boolean;
-  description?: string;
-  ownerId: string;
+export interface Module extends ModulePreview {
   cards: ICard[];
 }
 
 export interface ModulePreview {
   id: string;
   name: string;
+  public: boolean;
+  description?: string;
+  ownerId: string;
 }
