5106fd21a680dc9de47287081323ae959931e3c0
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-10 20:06:17 -0300

    feat: created AppLoader component to make a standard loader component

---
+++ components/app-loader.tsx
@@ -0,0 +1,7 @@
+export default function AppLoader() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-10 h-10 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
