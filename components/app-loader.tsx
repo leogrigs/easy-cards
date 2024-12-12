b12e95e09e6b034a2e980edfb724c881fd74722f
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-11 21:40:51 -0300

    chore: update app loader to use new logo

--- components/app-loader.tsx
+++ components/app-loader.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+import Logo from "../public/logo.png";
+
 export default function AppLoader() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 animate-pulse">
-      <div className="relative w-12 h-12">
-        <div className="absolute top-0 left-0 w-6 h-10 bg-indigo-700 rounded-md animate-hflip"></div>
-        <div className="absolute top-2 left-2 w-6 h-10 bg-indigo-500 rounded-md animate-hflip"></div>
-      </div>
+      <Image
+        className="animate-hflip"
+        src={Logo}
+        width={32}
+        height={32}
+        alt="Logo"
+      />
       <span className="text-indigo-400 text-md font-medium">Loading...</span>
     </div>
   );
