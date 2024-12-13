e8b2bb56f69ef3e7b5bdebfc63837143eab70487
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-12 21:41:20 -0300

    test: AppCard unit tests

---
+++ tests/components/AppCard.test.tsx
@@ -0,0 +1,27 @@
+import { AppCard } from "@/components/AppCard";
+import { ICard } from "@/interfaces/card.interface";
+import "@testing-library/jest-dom";
+import { fireEvent, render, waitFor } from "@testing-library/react";
+
+describe("AppCard component", () => {
+  const card: ICard = {
+    id: "1",
+    front: "Front text",
+    back: "Back text",
+  };
+
+  it("renders correctly", () => {
+    const { getByText } = render(<AppCard card={card} />);
+    expect(getByText(card.front)).toBeInTheDocument();
+    expect(getByText(card.back)).toBeInTheDocument();
+  });
+
+  it("flips when clicked", () => {
+    const { getByText, container } = render(<AppCard card={card} />);
+    const cardElement = container.querySelector(".cursor-pointer");
+    fireEvent.click(cardElement!);
+    waitFor(() =>
+      expect(getByText(card.back)).toHaveStyle("transform: rotateY(180deg);")
+    );
+  });
+});
